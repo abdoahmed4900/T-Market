85c1ff08c3a05f7c1dff982d81895c5022c0ef81
abdoahmed4900 <abdo.ahmed4900@gmail.com>
2025-09-17 10:40:17 +0300

    add firebase hosting workflow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home';
 
 export const routes: Routes = [
     {path: '',redirectTo: 'home', pathMatch: 'full' },
-    {path: 'home',},
-    {path: 'login',},
-    {path: 'register',},
-    {path: 'dashboard',},
-    {path: 'profile',},
-    {path : 'product/:id',},
-    {path : 'category/:id',},
-    {path: '**',redirectTo: 'home' }
-
+    {path: 'home',component: HomeComponent},
+    // {path: 'login',},
+    // {path: 'register',},
+    // {path: 'dashboard',},
+    // {path: 'profile',},
+    // {path : 'product/:id',},
+    // {path : 'category/:id',},
+    // {path: '**',redirectTo: 'home' }
 ];
