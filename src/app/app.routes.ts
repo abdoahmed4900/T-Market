79770e37063158a3d562f3f9e98d93c089fa106c
abdoahmed4900 <abdo.ahmed4900@gmail.com>
2025-11-07 14:24:05 +0200

    increase bundle size a little bit over 1mb for work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,43 @@
-import { LoginComponent } from './features/auth/login/login';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
-import { RegisterComponent } from './features/auth/register/register';
-import { ResetPassword } from './features/auth/reset-password/reset-password';
-import { ProductDetails } from './shared/product-details/product-details';
 import { Categories } from './features/categories/categories';
-import { CartComponent } from './features/cart/cart';
-import { Checkout } from './features/checkout/checkout';
 
 export const routes: Routes = [
-    {path: '',redirectTo: 'home', pathMatch: 'full' },
-    {path: 'home',component: HomeComponent},
-    {path: 'shop',component: Categories},
-    {path: 'login',component: LoginComponent},
-    {path: 'register',component: RegisterComponent},
-    {path: 'reset-password',component: ResetPassword},
-    {path: 'product/:id',component: ProductDetails},
-    {path: 'cart',component: CartComponent},
-    {path: 'checkout',component: Checkout},
+     { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: Categories },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login')
+      .then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register')
+      .then(m => m.RegisterComponent)
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () => import('./features/auth/reset-password/reset-password')
+      .then(m => m.ResetPassword)
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () => import('./shared/product-details/product-details')
+      .then(m => m.ProductDetails)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./features/cart/cart')
+      .then(m => m.CartComponent)
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./features/checkout/checkout')
+      .then(m => m.Checkout)
+  },
+
     // {path: 'dashboard',},
     // {path: 'profile',},
     // {path : 'product/:id',},
